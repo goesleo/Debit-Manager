b6d789b16554c12faf4983b54b474e7a1ec8b92f
leo goes <leonardotaqua20@hotmail.com>
2026-05-21 00:48:36 -0300

    feat: create REST controller and global exception handler for validations

---
+++ src/main/java/com/leonardo/debt_manager_api/controller/DividaController.java
@@ -0,0 +1,4 @@
+package com.leonardo.debt_manager_api.controller;
+
+public class DividaController {
+}
---
+++ src/main/java/com/leonardo/debt_manager_api/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,4 @@
+package com.leonardo.debt_manager_api.exceptions;
+
+public class GlobalExceptionHandler {
+}
